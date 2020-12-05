b483b4682b2483eeff231d6203bed5bcf64d8875
liangwei <dothegod@gmail.com>
2020-12-05 17:02:16 +0800

    配置参数

--- wind-admin/src/main.js
+++ wind-admin/src/main.js
@@ -27,18 +27,6 @@ import BaiduMap from 'vue-baidu-map'
 // fontawesome图标
 Object.keys(CustomComponents).forEach(key => Vue.use(CustomComponents[key]))
 
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online! ! !
- */
-// import { mockXHR } from '../mock'
-// if (process.env.NODE_ENV === 'production') {
-//   mockXHR()
-// }
 Vue.use(BaiduMap, {
   // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
   ak: ''
--- wind-admin/src/utils/request.js
+++ wind-admin/src/utils/request.js
@@ -12,7 +12,7 @@ axios.defaults.transformRequest = [function(data) {
 // create an axios instance
 const service = axios.create({
   baseURL: 'http://' + process.env.VUE_APP_BASE_API, // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
+  withCredentials: true, // send cookies when cross-domain requests
   timeout: 60000 // request timeout
 })
 
