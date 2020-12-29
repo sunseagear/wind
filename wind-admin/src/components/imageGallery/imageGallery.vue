cd472c8f6e4b1124f0e6fcdf4c2c3e855f2086d7
liangwei <dothegod@gmail.com>
2020-12-29 22:02:18 +0800

    更新組件

---
+++ wind-admin/src/components/imageGallery/imageGallery.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <el-row :gutter="gutter">
+      <el-col v-for="(item, index) in imageList" :key="index" :span="span">
+        <el-image :src="item" :preview-src-list="imageList" :style="{'margin-top': gutter}" style="width: 50px;height: 50px;" :fit="fit" />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ImageGallery',
+  props: {
+    value: {
+      type: [String, Array],
+      required: false,
+      default: ''
+    },
+    fit: {
+      type: String,
+      required: false,
+      default: 'fill'
+    },
+    span: {
+      type: Number,
+      required: false,
+      default: 8
+    },
+    width: {
+      type: String,
+      required: false,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      required: false,
+      default: '100%'
+    },
+    gutter: {
+      type: Number,
+      required: false,
+      default: 10
+    }
+  },
+  data() {
+    return {
+      imageList: []
+    }
+  },
+  watch: {
+    value: {
+      immediate: true,
+      handler(val) {
+        if (this.isNull(val)) {
+          this.imageList = []
+          return
+        }
+        if (val instanceof Array) {
+          this.imageList = val
+        } else {
+          this.imageList = val.split(',')
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
