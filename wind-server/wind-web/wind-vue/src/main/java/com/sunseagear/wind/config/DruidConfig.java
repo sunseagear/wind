3d8b94cba89a50efc2c7795564172fe0716fd1a5
liangwei <liangwei@gangwantech.com>
2021-08-02 16:50:56 +0800

    oracle适配

--- wind-server/wind-web/wind-vue/src/main/java/com/sunseagear/wind/config/DruidConfig.java
+++ wind-server/wind-web/wind-vue/src/main/java/com/sunseagear/wind/config/DruidConfig.java
@@ -1,11 +1,9 @@
 package com.sunseagear.wind.config;
 
-import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
 import org.apache.ibatis.mapping.DatabaseIdProvider;
 import org.apache.ibatis.mapping.VendorDatabaseIdProvider;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,28 +21,6 @@ import java.util.Properties;
  */
 @Configuration
 public class DruidConfig {
-
-    /**
-     * 注册一个StatViewServlet
-     *
-     * @return
-     */
-    @Bean
-    public ServletRegistrationBean druidStatViewServle() {
-        //org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/monitor/druid/*");
-        //添加初始化参数：initParams
-        //白名单：
-        //servletRegistrationBean.addInitParameter("allow","127.0.0.1");
-        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
-        //servletRegistrationBean.addInitParameter("deny","192.168.1.73");
-        //登录查看信息的账号密码.
-        //servletRegistrationBean.addInitParameter("loginUsername","admin2");
-        //servletRegistrationBean.addInitParameter("loginPassword","123456");
-        //是否能够重置数据.
-        //servletRegistrationBean.addInitParameter("resetEnable","false");
-        return servletRegistrationBean;
-    }
 
     /**
      * 注册一个：filterRegistrationBean
